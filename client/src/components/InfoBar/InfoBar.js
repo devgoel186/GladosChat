2e1c8608221df0c35a1294d0947d709581302865
Dev Goel <goeldev01@gmail.com>
2021-04-12 02:58:02 +0530

    Added InfoBar component to act as top status bar

---
+++ client/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import onlineIcon from "../../icons/onlineIcon.png";
+import closeIcon from "../../icons/closeIcon.png";
+
+import "./InfoBar.css";
+
+const InfoBar = ({ room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon} alt="Online" />
+      <h3>{room}</h3>
+    </div>
+    <div className="rightInnerContainer">
+      <a href="/">
+        <img src={closeIcon} alt="Close" />
+      </a>
+    </div>
+  </div>
+);
+
+export default InfoBar;
