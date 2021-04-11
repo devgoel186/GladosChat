81c2305703e6119e017e2e61232d4377567b6a27
Dev Goel <goeldev01@gmail.com>
2021-04-12 02:53:46 +0530

    Added chat functionality, emitted various events

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
+import "./Chat.css";
 
 let socket;
+const ENDPOINT = "localhost:5000";
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  const ENDPOINT = "localhost:5000";
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -18,14 +21,36 @@ const Chat = ({ location }) => {
     setRoom(room);
 
     socket.emit("join", { name, room }, () => {});
-
-    return () => {
-      socket.emit("disconnect");
-      socket.off();
-    };
   }, [ENDPOINT, location.search]);
 
-  return <h1>Chat</h1>;
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessages = (event) => {
+    event.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === "Enter" ? sendMessages(event) : null
+          }
+        ></input>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+
 const router = require("./router");
+const { addUser, getUser, removeUser, getUsersInRoom } = require("./users");
+
 const cors = require("cors");
 
 const app = express();
-
-app.use(cors());
-
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
@@ -17,21 +17,43 @@ const io = socketio(server, {
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(router);
 
-server.listen(PORT, () => {
-  console.log(`Server has started on port ${PORT}`);
-});
-
 io.on("connection", (socket) => {
-  console.log("New user connected");
-
+  console.log("Connected");
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
-    // if (err) callback({ error: err });
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name}, has joined!`,
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("Disconnected");
   });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
 });
